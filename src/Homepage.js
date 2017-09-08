8c5d290d069928522d9a47414fad102eafeb812a
github username <github email address>
2017-09-08 14:05:59 -0400

    actioncable working

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,6 +3,8 @@ import './App.css';
 import Logger from './components/Logger'
 import Lobby from './components/Lobby'
 import { Grid } from 'semantic-ui-react'
+import ActionCableProvider from 'react-actioncable-provider'
+import {ActionCable} from 'react-actioncable-provider'
 
 
 class Homepage extends Component {
@@ -57,16 +59,26 @@ class Homepage extends Component {
     }
   }
 
+  onReceived = (message) => {
+    console.log(message)
+  }
 
+  sendMessage = () => {
+    this.refs.roomChannel.perform('speak', {message:"hello"})
+  }
 
   render() {
     return (
       <div className="Homepage container">
+      <ActionCableProvider url='ws://localhost:3000/cable'>
         <Grid centered columns={3}>
             <Grid.Column>
               {this.loggedCheck()}
+              <ActionCable ref='roomChannel' channel={{channel: 'MatchChannel'}} onReceived={this.onReceived} />
+              <button onClick={this.sendMessage}>Send</button>
             </Grid.Column>
           </Grid>
+          </ActionCableProvider>
         </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Homepage from './Homepage';
 import registerServiceWorker from './registerServiceWorker';
+
 // import actionCable from 'actioncable'
 
 // const CableApp = {}
 //
 // CableApp.cable = actionCable.createConsumer(`ws://${window.location.hostname}:3000/cable`)
 
-ReactDOM.render(<Homepage />, document.getElementById('root'));
+ReactDOM.render(
+    <Homepage />
+  , document.getElementById('root'));
 registerServiceWorker();
